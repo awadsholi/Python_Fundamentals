ce27eef12f4fcea1078022af9ae98d2d7f790918
awadsholi <awad.sholi.1@gmail.com>
2025-03-23 02:01:35 +0200

    sss

--- Modules_Packages(Creating).py
+++
@@ -1,4 +0,0 @@
-from Test_Package import math
-
-print(math.add(3,6))
-print(math.subtract(7,4))
--- Type_Annotations_Modules.py
+++
@@ -1,9 +0,0 @@
-# Type Annotations help with code clarity and static type checking.
-from typing import List,Tuple
-
-def get_coordinates()-> Tuple[float,float]:
-    return (40.7128, -74.0060)
-
-def sum_numbers(numbers: List[int]) -> int:
-    return sum(numbers)
-
